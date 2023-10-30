9522c9e0c5bb1da9ed50d3c8b35eb76a3386fce2
golfergeek <golfergeet@gmail.com>
2023-10-30 12:08:12 -0500

    Update authorization module imports and guard in NestJS app, and fix API call in React app

--- apps/hiverarchy/hv-api-nest/src/app/app.module.ts
+++ apps/hiverarchy/hv-api-nest/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NestAuthorizationModule } from '@golfergeek/nest-authorization';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [NestAuthorizationModule, ConfigModule.forRoot({isGlobal: true})],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    NestAuthorizationModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      driver: ApolloDriver,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
